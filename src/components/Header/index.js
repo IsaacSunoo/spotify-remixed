6f25dbd3b76e7c615b4956653af574c35577305e
Isaac Sunoo <isaacsunoo@gmail.com>
2020-03-17 19:38:04 -0600

    init commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header className='main-header'>
+      <h1>Spotify Remixed</h1>
+    </header>
+  )
+}
+
+export default Header;
